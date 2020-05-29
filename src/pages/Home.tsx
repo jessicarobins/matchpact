053cfc452ed3012339a1bd9e01493a948136b34f
Jess Solka <jessrrobins@gmail.com>
2020-05-28 21:11:01 -0400

    Closes #1 initial setup of all the things

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+const Home: FC<{}> = () => {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Hero title</h1>
+          <h2 className="subtitle">Hero subtitle</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
