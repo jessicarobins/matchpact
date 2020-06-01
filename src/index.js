19d76daf2a837522336c416c23d8463210359893
Jess Solka <jessrrobins@gmail.com>
2020-05-31 21:30:43 -0400

    Closes #8 add sentry (#17)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as Sentry from '@sentry/browser';
 import * as firebase from 'firebase/app';
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
 import 'firebase/analytics';
-
-// Add the Firebase products that you want to use
 import 'firebase/auth';
 import 'firebase/firestore';
 
@@ -18,6 +13,10 @@ import './index.scss';
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+});
 
 ReactDOM.render(
   <React.StrictMode>
